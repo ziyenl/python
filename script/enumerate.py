49d0e13a3a6930c6a068594fd9d6538e30d47973
ziyenl <ziyenlerm@googlemail.com>
2016-10-05 05:05:13 +0800

    Update enumerate.py

--- script/enumerate.py
+++ script/enumerate.py
@@ -1,6 +1,6 @@
 __author__ = 'ziyenl'
 
-# Python's rule of thumb - when you need an index of a list use enumerate generator
+# when you need an index of a list use enumerate generator
 animals = ['dog', 'cat', 'bear', 'tiger']
 
 for animal in enumerate(animals, 1):
