25280d9c8919321935679efe1dcacc68b7705705
ziyenl <ziyenlerm@googlemail.com>
2016-10-10 13:16:30 +0800

    Update collections_counter.py

--- script/collections_counter.py
+++ script/collections_counter.py
@@ -2,24 +2,19 @@
 # https://www.hackerrank.com/challenges/collections-counter
 from collections import Counter
 
-number_of_shoes = input()
-available_shoe_sizes = input()
-number_of_customers = input()
+def get_total_earnings(shoe_sizes, customers):
+    '''Get total earnings'''
+    total_earnings = 0
+    counter = Counter(shoe_sizes)
+    for item in range(customers):
+        shoe_size, price = list( map(int, input().split(' ')) )
+        if shoe_size in counter.keys() and counter[shoe_size] > 0:
+            counter[shoe_size] -= 1
+            total_earnings += price
+    return total_earnings
 
-number_of_shoes = int(number_of_shoes)
-available_shoe_sizes = available_shoe_sizes.split(' ')
-available_shoe_sizes = [ int(item) for item in available_shoe_sizes ]
-number_of_customers = int(number_of_customers)
-counter = Counter(available_shoe_sizes)
-total_earnings = 0
-
-for item in range(number_of_customers):
-    shoe_size_and_price = input()
-    shoe_size_and_price = shoe_size_and_price.split(' ')
-    shoe_size, price = [ int(item) for item in shoe_size_and_price ]
-    if shoe_size in counter.keys() and counter[shoe_size] > 0:
-        counter[shoe_size] -= 1
-        total_earnings += price
-     
-print(total_earnings)
- 
+number_of_shoes = int(input())
+available_shoe_sizes = list( map(int, input().split(' ')) )
+number_of_customers = int(input())
+earnings = get_total_earnings(available_shoe_sizes, number_of_customers)
+print(earnings)
