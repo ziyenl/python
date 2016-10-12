f105c7fd16c55f3f17496f7e793fb0d099eb1808
ziyenl <ziyenlerm@googlemail.com>
2016-10-13 01:20:04 +0800

    Update decorators_phones.py

--- script/decorators_phones.py
+++ script/decorators_phones.py
@@ -2,22 +2,17 @@
 # https://www.hackerrank.com/challenges/standardize-mobile-number-using-decorators
 def format_phone_number(func):
     ''' Format phone number with a standardized prefix of +91 '''
-	def formatter(numbers):
-		formatted_numbers = []
-		for number in numbers:
-			if number.startswith('0'):
-				number = number[1:]
-			if len(number) >= 12:
-				number = number[3:] if number.startswith('+') else number[2:]
-			formatted_number = '+91 ' + number[:int(len(number)/2)] + ' ' + number[int(len(number)/2):]
-			formatted_numbers.append(formatted_number)
-		return func(formatted_numbers)
-	return formatter
+    def formatter(numbers):
+        formatted_numbers = []
+        for number in numbers:
+        	formatted_numbers.append('+91 ' + number[-10:-5] + ' ' + number[-5:])
+        return func(formatted_numbers)
+    return formatter
 
 
 @format_phone_number
 def print_phone_number(phone_numbers):
-    ''' Print phone number in sorted order '''
+	''' Print phone number in sorted order '''
 	for number in sorted(phone_numbers):
 		print( number )
 
@@ -26,5 +21,4 @@ phone_numbers = []
 for number in range(count):
     phone_numbers.append(raw_input())
  
-print_phone_number(phone_numbers)    
-  
+print_phone_number(phone_numbers)
