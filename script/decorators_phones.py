f88f89955a325b266d01cab91d0ed77e1995f195
ziyenl <ziyenlerm@googlemail.com>
2016-10-13 01:01:39 +0800

    Update decorators_phones.py

--- script/decorators_phones.py
+++ script/decorators_phones.py
@@ -8,7 +8,7 @@ def format_phone_number(func):
 			if len(number) >= 12:
 				number = number[3:] if number.startswith('+') else number[2:]
 			formatted_number = '+91 ' + number[:int(len(number)/2)] + ' ' + number[int(len(number)/2):]
-			formatted_numbers.append( formatted_number )
+			formatted_numbers.append(formatted_number)
 		return func(formatted_numbers)
 	return formatter
 
@@ -22,7 +22,7 @@ def print_phone_number(phone_numbers):
 count = int(raw_input())
 phone_numbers = []
 for number in range(count):
-    phone_numbers.append( raw_input() )
+    phone_numbers.append(raw_input())
  
 print_phone_number(phone_numbers)    
   
