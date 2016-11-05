9e5d9a56f7f9b5da4d5e16d7168b8f8f22a77511
ziyenl <ziyenlerm@googlemail.com>
2016-11-05 14:44:34 +0000

    Create cycle_detection.py

---
+++ script/cycle_detection.py
@@ -0,0 +1,36 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+
+# Storage method
+def has_cycle(head):
+    checked_list = []
+    current_node = head
+    while current_node:
+        if current_node in checked_list:
+            return True
+        checked_list.append( current_node )
+        current_node = current_node.next
+    return False
+
+# Floyd's cycle finding hare and tortoise
+def has_cycle(head):
+    if head is None: 
+        return False
+    tortoise = head 
+    hare = head.next
+    while(tortoise is not None and hare is not None and hare.next is not None):
+        if tortoise == hare:
+            return True
+        tortoise = tortoise.next
+        hare = hare.next.next
+    return False
+    
