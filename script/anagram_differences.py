be5c9ad0bf3afecbe6746dded3d98e59558657a7
ziyenl <ziyenlerm@googlemail.com>
2016-11-05 21:27:44 +0000

    Create anagram_differences.py

---
+++ script/anagram_differences.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def number_needed(a, b):
+	common_total = 0
+	counter_a = Counter(a)
+	counter_b = Counter(b)
+	for item in counter_b:
+		if item in counter_a.iterkeys():
+			common_total += min(counter_a[item], counter_b[item])
+	differences = len(a) + len(b) - common_total * 2
+	return differences
+
+a = raw_input().strip()
+b = raw_input().strip()
+
+print number_needed(a, b)
