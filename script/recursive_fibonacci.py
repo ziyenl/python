3b9cf2d186bd87a20ccb57152226bf76d9f5d0bc
ziyenl <ziyenlerm@googlemail.com>
2016-11-06 13:41:47 +0000

    Update recursive_fibonacci.py

--- script/recursive_fibonacci.py
+++ script/recursive_fibonacci.py
@@ -1,23 +1,21 @@
 # This is solution for recursive fibonacci using memoize
 # https://www.hackerrank.com/challenges/ctci-fibonacci-numbers
 def memoize(func):
-    ''' memoize fibonacci functions '''
     storage = {}
     def helper(n):
         if n not in storage.keys():
-            storage[n] = func(n)
+            storage[n]=func(n)
         return storage[n]
     return helper
 
+@memoize
 def fibonacci(n):
-    ''' recursive fibonacci calculation '''
-    if n == 0 or n == 1:
+    if n==0 or n==1:
         return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
         
 n = int(raw_input())
-fibonacci = memoize(fibonacci)
 print(fibonacci(n))
 
 
