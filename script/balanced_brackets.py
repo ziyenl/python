861f3b61812f98168d333e6c71e1afe2f23bfa0d
ziyenl <ziyenlerm@googlemail.com>
2016-11-06 10:56:28 +0000

    Create balanced_brackets.py

---
+++ script/balanced_brackets.py
@@ -0,0 +1,21 @@
+def is_matched(expression):
+    ''' check if expression matches '''
+    stack = []
+    mapping = { '{' : '}', '[' : ']', '(' : ')'}
+  
+    for expr in expression:
+        if expr in mapping.keys():
+            stack.append(mapping[expr])
+        else:
+            if len(stack) == 0 or stack.pop() <> expr:
+                return False
+    if len(stack) <> 0: return False
+    return True
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+    expression = raw_input().strip()
+    if is_matched(expression) == True:
+        print "YES"
+    else:
+        print "NO"
