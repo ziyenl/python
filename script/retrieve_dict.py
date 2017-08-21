5e71f750fe2d081f0f9b767c80eb47afb3f4a7f7
ziyenl <ziyenlerm@googlemail.com>
2017-08-21 18:41:21 +0100

    Update retrieve_dict.py

--- script/retrieve_dict.py
+++ script/retrieve_dict.py
@@ -4,14 +4,24 @@ sample_dict = {'apple' : 3, 'pear: 5, 'orange : 6 }
 
 # Method 1: In Dictionary
 retrieve_item = 'apple'
-if  retrieve_item in sample_dict:
-  return sample_dict[retrieve_item]
-else:
-  print('{} not found'.format( retrieve_item )
+def get_value(retrieve_item):
+  if  retrieve_item in sample_dict:
+    print( sample_dict[retrieve_item] )
+  else:
+    print('{} not found'.format( retrieve_item ))
   
 # Method 2: Try/Except
-if  retrieve_item in sample_dict:
-  return sample_dict[retrieve_item]
-else:
-  print('{} not found'.format( retrieve_item )
+def get_value(retrieve_item):
+  try:
+    print( sample_dict[retrieve_item] )
+  except:
+    print('{} not found'.format( retrieve_item ))
+          
+# Default Value of dict.get()
+def get_value(retrieve_item):
+  default = object()
+  val = sample_dict.get(retrieve_item, default)
+  if val is default:
+    print('{} not found'.format( retrieve_item))
+  
   
