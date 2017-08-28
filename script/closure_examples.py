d3fbf9af9b0d0be7c2a2fb12efb1c6944297f973
ziyenl <ziyenlerm@googlemail.com>
2017-08-28 10:58:10 +0100

    Update closure_examples.py

--- script/closure_examples.py
+++ script/closure_examples.py
@@ -14,16 +14,16 @@ print(list_numbers)
 
 def group_by_numbers_with_closure(number):
     group = {4, 5, 6}
-    found = False
+    exist = False
     def helper(number):
         if number in group:
             nonlocal present
-            present = True
+            exist = True
             return 0, number
         else:
             return 1, number
     number.sort(key=helper)
-    return found
+    return exist
 
 sample_list = [1,2,3,4,5,6]
 print(group_by_numbers(sample_list))
