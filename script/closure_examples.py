bb240043b4fea52451921acf40c79e99246307a6
ziyenl <ziyenlerm@googlemail.com>
2017-08-28 10:57:13 +0100

    Update closure_examples.py

--- script/closure_examples.py
+++ script/closure_examples.py
@@ -11,3 +11,21 @@ list_numbers.sort(key=group_by_numbers)
 
 print(list_numbers)
 
+
+def group_by_numbers_with_closure(number):
+    group = {4, 5, 6}
+    found = False
+    def helper(number):
+        if number in group:
+            nonlocal present
+            present = True
+            return 0, number
+        else:
+            return 1, number
+    number.sort(key=helper)
+    return found
+
+sample_list = [1,2,3,4,5,6]
+print(group_by_numbers(sample_list))
+print(sample_list)
+
