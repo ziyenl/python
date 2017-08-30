ffaf2c3cab06d7a49e4499f33e0d0e747be47c58
ziyenl <ziyenlerm@googlemail.com>
2017-08-30 20:22:52 +0100

    Create trader_profit.py

---
+++ script/trader_profit.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import sys
+
+
+def traderProfit(k, n, A):
+    if k == 0:
+        return 0
+    current_best = 0
+    for start in range(len(A)-1):
+        for end in range(start+1, len(A)):
+            if A[end] > A[start]:
+                diff = A[end] - A[start]
+                diff = diff + traderProfit(k-1, n, A[end+1:])
+                current_best = max(diff, current_best)
+    return current_best
+
+
+if __name__ == "__main__":
+    q = int(input().strip())
+    for a0 in range(q):
+        k = int(input().strip())
+        n = int(input().strip())
+        arr = list(map(int, input().strip().split(' ')))
+        result = traderProfit(k, n, arr)
+        print(result)
